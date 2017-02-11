774fe87a31d75a5e9e7afc6c6577c1f70d18557e
Adam <adam@lesniak.pl>
2017-02-11 23:26:56 +0000

    App structure and search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/SearchBar';
+
+const API_KEY = process.env.YouTube_DATA_API;
+
 // Create a new component
 // This component should produce some HTML
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Write it out to DO
