11dfb1847eea97147e01b736058741b09797e0fc
Adam <adam@lesniak.pl>
2017-02-11 21:30:40 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component
+// This component should produce some HTML
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// Write it out to DO
+ReactDOM.render(<App />, document.querySelector('.container'));
